1bd1f9b528098d1812dcad962d47098365598a95
Matteo Pioldi <matteopioldi@MacBook-Pro-di-Matteo.local>
2024-03-10 21:21:46 +0100

    root to numpy traducer

--- tests/dummytree.py
+++ tests/dummytree.py
@@ -3,40 +3,37 @@ import os
 from array import array
 import ROOT as root
 
-def MakeDummyTree(M, a, b): #makes a tree with M events, two columns, one with vectors of a elements and the other with vectors of b elements
-
-    name = "dummytree.root"
+def MakeDummyTree(M, name = "dummytree.root"): #makes a tree with M events, two columns, one with vectors of a elements and the other with vectors of b elements, filled with consecutive numbers
 
     path = "./" + name
 
     if os.path.exists(path):
 
-        os.remove(path)
+        os.remove(path) #if the file containing the dummy tree already exists, it is canceled
 
     file = root.TFile.Open(name, "RECREATE")
 
     tree = root.TTree("tree", "tree")
 
-    paramvector = array('d', a*[0.]) 
-    covariancematrix = array('d', b*[0.])
+    #paramvector = array('d', a*[0.]) 
+    #covariancematrix = array('d', b*[0.])
 
-    tree.Branch("Vector", paramvector, 'Vector/D')
-    tree.Branch("Matrix", covariancematrix, 'Matrix/D')
+    paramvector = np.zeros((5,), dtype=np.double)
+    covariancematrix = np.zeros((15,), dtype=np.double)
+
+    tree.Branch("Vector", paramvector, 'Vector[5]/D')
+    tree.Branch("Matrix", covariancematrix, 'Matrix[15]/D')
 
     for i in range(M):
 
-        for j in range(a):
+        for j in range(5):
 
-            paramvector[j] =  i*(a+b) + j
+            paramvector[j] =  i*20 + j
 
-        for k in range(b):
+        for k in range(15):
 
-            covariancematrix[k] = i*(a+b) + a + k
+            covariancematrix[k] = i*20 + 5 + k
 
         tree.Fill()
 
     file.Write()
-
-if __name__ == '__main__':
-
-    unittest.main()
