5b3ce4681f1bd3c79a2190be71caeb438bf03bb4
mpioldi <146322233+mpioldi@users.noreply.github.com>
2024-06-30 21:32:44 +0200

    Added save in txt and fixed graphs in decompressor

--- code/decompressor.py
+++ code/decompressor.py
@@ -39,7 +39,7 @@ def DataDecompressor(fname, n_bins, img=0):
     print('started model definition')
         
     #model definition
-        
+    
     model = RealNVP(num_coupling_layers=layers_number) #compiling model
     model.compile()
     model.model_load_weights()
@@ -53,30 +53,41 @@ def DataDecompressor(fname, n_bins, img=0):
     #uploading data
 
     #load data in numpy tensor z_unif...
+    z_unif = np.loadtxt(fname, skiprows=1)
 
     print('data imported')
+    
+    if img == 1:
+    
+        # Generate 16 plots in a 4x4 arrangement after switching to uniform
+        f2, axes2 = plt.subplots(4, 4)
+        f2.set_size_inches(20, 15)
+
+        for i in range(4):
+            #print(f'i={i}')
+            for j in range(4):
+                k = 4 + i*4 + j
+                '''
+                print(f'j={j}')
+                print(f'k={k}')
+                '''
+                if k!=4:
+                    axes2[i, j].hist(z_unif[:, k], range=(0, int(n_bins)), bins=500, color="r")
+                    axes2[i, j].set(title=f"{k}")
+
+                    
+        #plt.savefig('uniform.png')
 
     # convert to gaussian
 
     z = z_unif
-    z[5:] = gauss(z[5:]) / float(n_bins)
+    z[:, 5:] = gauss((z[:, 5:] + 0.5)/ float(n_bins))
+    z = z.astype('float32')
     z = tf.convert_to_tensor(z)
 
     # From gaussian to normalized data
     data, _ = model(z, training=False)
-
-    print('data converted')
-
-    # normalization inversion
-
-    inv_norm = keras.layers.Normalization(axis=-1, mean=mus, variance=sigmas ** 2, invert=True)
-    data = inv_norm(data)
-
-    print('data recovered')
-
-
-
-    #plots, if requested:
+    
     if img == 1:
     
         # From latent space to data: data reconstruction (x) from Gaussian (sample)
@@ -104,9 +115,24 @@ def DataDecompressor(fname, n_bins, img=0):
                 print(f'k={k}')
                 '''
                 if k!=4:
-                    axes[i, j].hist(data[:, k], range=(-1,1), bins=500, color="r")
+                    axes[i, j].hist(data[:, k], bins=500, range=(-0.5,0.5), color="r")
                     
-        plt.savefig('precompressed.png')
+        plt.savefig('decompressed.png')
+
+    print('data converted')
+
+    # normalization inversion
+
+    inv_norm = keras.layers.Normalization(axis=-1, mean=mus, variance=sigmas ** 2, invert=True)
+    data = inv_norm(data)
+
+
+    print('data recovered')
+
+    
+
+    #plots, if requested:
+    if img == 1:
 
     
         # Generate 16 plots in a 4x4 arrangement after change of coordinates
@@ -128,50 +154,11 @@ def DataDecompressor(fname, n_bins, img=0):
                 print(f'k={k}')
                 '''
                 if k!=4:
-                    axes1[i, j].hist(z[:, k], range=(-4,4), bins=500, color="r")
+                    axes1[i, j].hist(z[:, k], bins=500, range=(-4, 4), color="r")
                     axes1[i, j].set(title=f"{k}")
 
                     
-        plt.savefig('gaussians.png')
-
-    
-        # Generate 16 plots in a 4x4 arrangement after switching to uniform
-        f2, axes2 = plt.subplots(4, 4)
-        f2.set_size_inches(20, 15)
-
-        for i in range(4):
-            #print(f'i={i}')
-            for j in range(4):
-                k = 4 + i*4 + j
-                '''
-                print(f'j={j}')
-                print(f'k={k}')
-                '''
-                if k!=4:
-                    axes2[i, j].hist(z_unif[:, k], range=(0, int(n_bins)), bins=500, color="r")
-                    axes2[i, j].set(title=f"{k}")
-
-                    
-        plt.savefig('uniform.png')
-
-    
-        # Generate 16 plots in a 4x4 arrangement for z_compr
-        f3, axes3 = plt.subplots(4, 4)
-        f3.set_size_inches(20, 15)
-
-        for i in range(4):
-            #print(f'i={i}')
-            for j in range(4):
-                k = 4 + i*4 + j
-                '''
-                print(f'j={j}')
-                print(f'k={k}')
-                '''
-                if k!=4:
-                    axes3[i, j].hist(z_compr[:, k], range=(0, int(n_bins)), bins=500, color="r")
-                    axes3[i, j].set(title=f"{k}")
-
-        plt.savefig('compressed.png')
+        #plt.savefig('gaussians.png')
 
         #show graphs
 
@@ -180,10 +167,16 @@ def DataDecompressor(fname, n_bins, img=0):
     return data
 
 if __name__ == '__main__':
-    name = "data.txt"
-    n = 500
+    name = "compr_data.txt"
+    n = 2048
     result = DataDecompressor(name, n, img=produce_images)
+    np.savetxt("decompr_data.txt", result, delimiter=' ', newline='\n', header='')
     
+    
+       
+    
+
+
     
     
     
