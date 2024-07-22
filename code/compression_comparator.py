0d2d5702a3d8f43050b6ff35caccd3468080db9d
mpioldi <146322233+mpioldi@users.noreply.github.com>
2024-07-22 10:27:51 +0200

    Fixed graph in compression_comparator.py

--- code/compression_comparator.py
+++ code/compression_comparator.py
@@ -41,9 +41,12 @@ data = np.loadtxt(fname, skiprows=1)
 data = data[::500]
 
 
-alt_compr_size = np.zeros(14)
-alt_compr_accuracy = np.zeros(14)
-alt_compr_accuracy_std = np.zeros(14)
+alt_compr_size = np.zeros(20)
+alt_compr_accuracy = np.zeros(20)
+alt_compr_accuracy_std = np.zeros(20)
+alt_compr_annotation = np.zeros(20)
+
+n=0
 
 for cut in list(range(0, 45, 4)) + list(range (45, 53, 1)):
 
@@ -53,18 +56,22 @@ for cut in list(range(0, 45, 4)) + list(range (45, 53, 1)):
     if not os.path.exists(path):
         AltDataCompressor("data.txt", savefile, cut)
 
-    n = int(cut/4)
+    #n = int(cut/4)
 
     alt_compr_size[n] = os.path.getsize(path)
 
     alt_decompr_data = bit_decompressor(savefile)
     alt_decompr_data = alt_decompr_data[0, 0]
     alt_compr_accuracy[n], alt_compr_accuracy_std[n] = comparator(data, alt_decompr_data)
+    alt_compr_annotation[n] = cut
+    
+    n+=1
 
 
 compr_size = np.zeros(17)
 compr_accuracy = np.zeros(17)
 compr_accuracy_std = np.zeros(17)
+compr_annotation = np.zeros(17)
 
 for n_bins in range(0, 4097, 256):
 
@@ -93,11 +100,24 @@ for n_bins in range(0, 4097, 256):
 
     decompr_data = np.loadtxt(decompr_savefile)
     compr_accuracy[n], compr_accuracy_std[n] = comparator(data, decompr_data)
+    compr_annotation[n] = n*256
 
 
 plt.figure(figsize=(15, 10))
-plt.errorbar(compr_size, compr_accuracy, compr_accuracy_std, fmt='o')
-plt.errorbar(alt_compr_size, alt_compr_accuracy, alt_compr_accuracy_std, fmt='o')
+plt.errorbar(compr_size, compr_accuracy, compr_accuracy_std, fmt='.')
+
+'''
+for i, (xi, yi) in enumerate(zip(compr_size, compr_accuracy)):
+    plt.annotate(f'{int(compr_annotation[i])}', (xi, yi), textcoords="offset points", xytext=(0,10), ha='center')
+'''
+
+plt.errorbar(alt_compr_size, alt_compr_accuracy, alt_compr_accuracy_std, fmt='.')
+
+'''
+for i, (xi, yi) in enumerate(zip(alt_compr_size, alt_compr_accuracy)):
+    plt.annotate(f'{int(alt_compr_annotation[i])}', (xi, yi), textcoords="offset points", xytext=(s0,10), ha='center')
+'''
+
 plt.title("compression accuracy")
 plt.legend(["NVP compr.", "Alt. compr."], loc="upper right")
 plt.ylabel("accuracy")
